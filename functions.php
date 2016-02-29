e808468b4e15195bbc1aad759ad07f4558b08926
Gnieark <gnieark@free.fr>
2016-02-29 12:22:36 +0100

    presque rien

--- functions.php
+++ functions.php
@@ -3,3 +3,8 @@
 function login_page(){
   return (preg_replace( '/\{\{title\}\}/', SITE_TITLE, file_get_contents('../tpl/connexion.htm'))); 
 }
+function connect(){
+
+
+
+}
--- www/index.php
+++ www/index.php
@@ -6,7 +6,25 @@ if(!file_exists('../config.php')){
 }else{
   include ('../config.php');
 }
+if((isset($_POST['act'])) && ($_POST['act']=="connect")){
+  //the only thing we can do without being connected is to try to connect
+  connect();
+}
 if(!isset($_SESSION['isConnected'])){
         echo login_page();
         die;
 }
+
+if(isset($_GET['menu']))
+  $menu=$_GET['menu'];
+else
+  $menu="";
+
+switch($menu){
+  case "session":
+  
+    break;
+  default:
+    break;
+}
+
